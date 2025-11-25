b2ad85c94089aee348b6d184ef1ab98026be62dc
iamranjith04 <ranjithnataraj04@gmail.com>
2025-11-25 19:52:20 +0530

    file name changed for database

---
+++ src/main/java/com/example/demo/Database/DemoDatabaseOptimal.java
@@ -0,0 +1,66 @@
+package com.example.demo.Database;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Version;
+
+@Entity
+@Table(name = "demoDatabaseForOptimalLocking")
+public class DemoDatabaseOptimal{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private int accountBalance;
+    
+    @Version
+    private int version;
+
+    public DemoDatabaseOptimal() {
+    }
+
+    public DemoDatabaseOptimal(int id, String name, int accountBalance, int version) {
+        this.id = id;
+        this.name = name;
+        this.accountBalance = accountBalance;
+        this.version = version;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setAccountBalance(int accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+    
+}
